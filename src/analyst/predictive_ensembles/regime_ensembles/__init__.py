db0e59b0524b04c75b7465bf27b59913063a8f94
hydror <remy.roche75@gmail.com>
2025-07-28 12:23:37 +0200

    Create __init__.py

---
+++ src/analyst/predictive_ensembles/regime_ensembles/__init__.py
@@ -0,0 +1,10 @@
+# src/analyst/predictive_ensembles/regime_ensembles/__init__.py
+
+# Import all specific ensemble classes here for easier access
+from .base_ensemble import BaseEnsemble
+from .bull_trend_ensemble import BullTrendEnsemble
+from .bear_trend_ensemble import BearTrendEnsemble
+from .sideways_range_ensemble import SidewaysRangeEnsemble
+from .sr_zone_action_ensemble import SRZoneActionEnsemble
+from .high_impact_candle_ensemble import HighImpactCandleEnsemble
+

06f0cdf940c44b1da683047caf438b3b90352ef5
hydror <remy.roche75@gmail.com>
2025-07-28 11:07:04 +0200

    Create __init__.py

---
+++ src/supervisor/__init__.py
@@ -0,0 +1,5 @@
+# src/supervisor/__init__.py
+# This file makes the 'supervisor' directory a Python package.
+
+# You can import sub-modules here for easier access, e.g.:
+# from .supervisor import Supervisor

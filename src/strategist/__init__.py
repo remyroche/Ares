22d9d2acbaef6b6573f6c5cb280e419fe13d1235
hydror <remy.roche75@gmail.com>
2025-07-28 11:02:10 +0200

    Create __init__.py

---
+++ src/strategist/__init__.py
@@ -0,0 +1,5 @@
+# src/strategist/__init__.py
+# This file makes the 'strategist' directory a Python package.
+
+# You can import sub-modules here for easier access, e.g.:
+# from .strategist import Strategist

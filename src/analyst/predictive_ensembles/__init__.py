17f5feafe92a4a880d2ae1e8192105f105a48785
hydror <remy.roche75@gmail.com>
2025-07-28 12:23:16 +0200

    Create __init__.py

---
+++ src/analyst/predictive_ensembles/__init__.py
@@ -0,0 +1,6 @@
+# src/analyst/predictive_ensembles/__init__.py
+
+from .ensemble_orchestrator import RegimePredictiveEnsembles
+# You can also import specific ensemble classes if needed elsewhere, e.g.:
+# from .regime_ensembles.bull_trend_ensemble import BullTrendEnsemble
+

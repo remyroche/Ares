9291cf25fce4282ba8e548c9d4e5730b45660199
hydror <remy.roche75@gmail.com>
2025-11-10 16:21:52 +0100

    Delete src/training/steps/market_analysis/regime_analysis directory

--- src/training/steps/market_analysis/regime_analysis/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Regime analysis services and utilities."""
-
-from .service import RegimeAnalysisService
-from . import label_fusion
-
-__all__ = [
-    "label_fusion",
-    "RegimeAnalysisService"
-]
